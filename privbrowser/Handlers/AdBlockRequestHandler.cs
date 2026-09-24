09c950bdfef9334c8d73c581179b92cc8709f93f
Andarstudio <tomenogamestore@gmail.com>
2026-09-24 15:03:24 +0800

    Update AdBlockRequestHandler.cs

--- privbrowser/Handlers/AdBlockRequestHandler.cs
+++ privbrowser/Handlers/AdBlockRequestHandler.cs
@@ -1,57 +1,56 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using CefSharp;
+using CefSharp.Handler;
 
 namespace PrivBrowser.Handlers
 {
-    public class AdBlockRequestHandler : IRequestHandler
+    public class AdBlockRequestHandler : RequestHandler
     {
-        private static readonly HashSet<string> BlockedDomains = new(StringComparer.OrdinalIgnoreCase)
-        {
-            "google-analytics.com",
-            "analytics.google.com",
-            "doubleclick.net",
-            "googlesyndication.com",
-            "adservice.google.com",
-            "facebook.net/connect",
-            "pixel.facebook.com",
-            "scorecardresearch.com",
-            "quantserve.com",
-            "outbrain.com",
-            "taboola.com",
-            "adnxs.com",
-            "criteo.com",
-            "telemetry.microsoft.com"
-        };
+        public bool TrackerProtectionEnabled { get; set; } = true;
 
-        public bool OnBeforeBrowse(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool userGesture, bool isRedirect) => false;
-        public bool OnOpenUrlFromTab(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture) => false;
-
-        public IResourceRequestHandler GetResourceRequestHandler(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool isNavigation, bool isDownload, string requestInitiator, ref bool disableDefaultHandling)
+        protected override IResourceRequestHandler GetResourceRequestHandler(
+            IWebBrowser chromiumWebBrowser,
+            IBrowser browser,
+            IFrame frame,
+            IRequest request,
+            bool isNavigation,
+            bool isDownload,
+            string requestInitiator,
+            ref bool disableDefaultHandling)
         {
-            if (Uri.TryCreate(request.Url, UriKind.Absolute, out Uri? uri))
-            {
-                if (BlockedDomains.Any(domain => uri.Host.EndsWith(domain, StringComparison.OrdinalIgnoreCase)))
-                {
-                    return new BlockResourceRequestHandler();
-                }
-            }
-            return null!;
+            return new AdBlockResourceRequestHandler(TrackerProtectionEnabled);
         }
 
-        public bool GetAuthCredentials(IWebBrowser chromiumWebBrowser, IBrowser browser, string originUrl, bool isProxy, string host, int port, string realm, string scheme, IAuthCallback callback) => true;
-        public bool OnQuotaRequest(IWebBrowser chromiumWebBrowser, IBrowser browser, string originUrl, long newSize, IRequestCallback callback) => true;
-        public bool OnCertificateError(IWebBrowser chromiumWebBrowser, IBrowser browser, CefErrorCode errorCode, string requestUrl, ISslInfo sslInfo, IRequestCallback callback) => false;
-        public void OnRenderProcessTerminated(IWebBrowser chromiumWebBrowser, IBrowser browser, CefTerminationStatus status) { }
-        public void OnDocumentAvailableInMainFrame(IWebBrowser chromiumWebBrowser, IBrowser browser) { }
-    }
-
-    public class BlockResourceRequestHandler : ResourceRequestHandler
-    {
-        protected override CefReturnValue OnBeforeResourceLoad(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, IRequestCallback callback)
+        private class AdBlockResourceRequestHandler : ResourceRequestHandler
         {
-            return CefReturnValue.Cancel;
+            private readonly bool _enabled;
+            private static readonly string[] BlockedDomains = new[] { "analytics.", "telemetry.", "doubleclick.net", "adservice." };
+
+            public AdBlockResourceRequestHandler(bool enabled)
+            {
+                _enabled = enabled;
+            }
+
+            protected override CefReturnValue OnBeforeResourceLoad(
+                IWebBrowser chromiumWebBrowser,
+                IBrowser browser,
+                IFrame frame,
+                IRequest request,
+                IRequestCallback callback)
+            {
+                if (_enabled)
+                {
+                    string url = request.Url.ToLowerInvariant();
+                    foreach (var domain in BlockedDomains)
+                    {
+                        if (url.Contains(domain))
+                        {
+                            return CefReturnValue.Cancel;
+                        }
+                    }
+                }
+                return CefReturnValue.Continue;
+            }
         }
     }
 }
